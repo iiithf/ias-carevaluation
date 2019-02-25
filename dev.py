c4685bc8ed54387ea86b5a6b6ca19aced34db778
Subhajit Sahu <wolfram77@gmail.com>
2019-02-26 04:50:41 +0530

    software engineered code

--- dev.py
+++ dev.py
@@ -7,35 +7,46 @@ import csv
 import os
 
 
+
 MODEL = 'build'
-NBUYING = ['vhigh', 'high', 'med', 'low']
-NMAINT = ['vhigh', 'high', 'med', 'low']
-NDOORS = ['2', '3', '4', '5more']
-NPERSONS = ['2', '4', 'more']
-NLUG_BOOT = ['small', 'med', 'big']
-NSAFETY = ['low', 'med', 'high']
-NACCEPT = ['unacc', 'acc', 'good', 'vgood']
-TBUYING = tf.contrib.lookup.index_table_from_tensor(tf.constant(NBUYING))
-TMAINT = tf.contrib.lookup.index_table_from_tensor(tf.constant(NMAINT))
-TDOORS = tf.contrib.lookup.index_table_from_tensor(tf.constant(NDOORS))
-TPERSONS = tf.contrib.lookup.index_table_from_tensor(tf.constant(NPERSONS))
-TLUG_BOOT = tf.contrib.lookup.index_table_from_tensor(tf.constant(NLUG_BOOT))
-TSAFETY = tf.contrib.lookup.index_table_from_tensor(tf.constant(NSAFETY))
+DATASET = 'car.data'
+INPUTS = {
+  'buying': ['vhigh', 'high', 'med', 'low'],
+  'maint': ['vhigh', 'high', 'med', 'low'],
+  'doors': ['2', '3', '4', '5more'],
+  'persons': ['2', '4', 'more'],
+  'lug_boot': ['small', 'med', 'big'],
+  'safety': ['low', 'med', 'high']
+}
+OUTPUT = ['unacc', 'acc', 'good', 'vgood']
+FEATURES = list(INPUTS.keys())
+INPS = len(INPUTS)
+OUTS = len(OUTPUT)
+RATE = 0.1
+EPOCHS = 200
 
 
-def attrib_numbers(row):
-  buying = float(NBUYING.index(row[0]))
-  maint = float(NMAINT.index(row[1]))
-  doors = float(NDOORS.index(row[2]))
-  persons = float(NPERSONS.index(row[3]))
-  lug_boot = float(NLUG_BOOT.index(row[4]))
-  safety = float(NSAFETY.index(row[5]))
-  return [buying, maint, doors, persons, lug_boot, safety]
 
-def class_numbers(accept):
-  numbers = [0.0, 0.0, 0.0, 0.0]
-  numbers[NACCEPT.index(accept)] = 1.0
+def lookup_table(lst):
+  return tf.contrib.lookup.index_table_from_tensor(tf.constant(lst))
+
+def input_numbers(row):
+  numbers = np.zeros(6)
+  for i in range(len(FEATURES)):
+    numbers[i] = float(INPUTS[FEATURES[i]].index(row[i]))
   return numbers
+
+def output_numbers(accept):
+  numbers = np.zeros(4)
+  numbers[OUTPUT.index(accept)] = 1.0
+  return numbers
+
+def input_tables():
+  tables = {}
+  for k in INPUTS:
+    tables[k] = tf.contrib.lookup.index_table_from_tensor(tf.constant(INPUTS[k]))
+  return tables
+
 
 def ann_layer(x, size, name=None):
   w = tf.Variable(tf.truncated_normal(size))
@@ -53,66 +64,48 @@ def get_data(name, test_per):
   x, y = ([], [])
   with open(name, 'r') as f:
     for row in csv.reader(f):
-      x.append(attrib_numbers(row))
-      y.append(class_numbers(row[6]))
+      x.append(input_numbers(row))
+      y.append(output_numbers(row[6]))
   x, y = shuffle(x, y)
   return train_test_split(x, y, test_size=test_per)
 
 def input_tensors(x):
   return {'inputs': tf.saved_model.build_tensor_info(x)}
 
-def classify_signature(x, y):
-  inputs = {'inputs': tf.saved_model.utils.build_tensor_info(serialized_tf_example)}
-  outputs_classes = tf.saved_model.utils.build_tensor_info(prediction_classes)
-  outputs_scores = tf.saved_model.utils.build_tensor_info(values)
-  outputs = {'classes': outputs_classes, 'scores': outputs_scores}
-  return tf.saved_model.build_signature_def(input_tensors(x), outputs, 'tensorflow/serving/classify')
+def classify_signature(x_serialized, y_classes, y_values):
+  inputs = {'inputs': tf.saved_model.utils.build_tensor_info(x_serialized)}
+  classes = tf.saved_model.utils.build_tensor_info(y_classes)
+  scores = tf.saved_model.utils.build_tensor_info(y_values)
+  outputs = {'classes': classes, 'scores': scores}
+  return tf.saved_model.build_signature_def(inputs, outputs, 'tensorflow/serving/classify')
 
 def predict_signature(x, y):
+  inputs = {'inputs': tf.saved_model.build_tensor_info(x)}
   outputs = {'scores': tf.saved_model.build_tensor_info(y)}
-  return tf.saved_model.build_signature_def(input_tensors(x), outputs, 'tensorflow/serving/predict')
+  return tf.saved_model.build_signature_def(inputs, outputs, 'tensorflow/serving/predict')
 
 
-print('reading dataset:')
-inps, outs = (6, 4)
-rate, epochs = (0.1, 200)
-train_x, test_x, train_y, test_y = get_data('car.data', 0.2)
+
+print('reading %s:' % DATASET)
+train_x, test_x, train_y, test_y = get_data(DATASET, 0.2)
 print('%d train rows, %d test rows' % (len(train_x), len(test_x)))
 
 print('\ndefining ann:')
-serialized_tf_example = tf.placeholder(tf.string, name='tf_example')
-feature_configs = {
-  'buying': tf.FixedLenFeature(shape=1, dtype=tf.string),
-  'maint': tf.FixedLenFeature(shape=1, dtype=tf.string),
-  'doors': tf.FixedLenFeature(shape=1, dtype=tf.string),
-  'persons': tf.FixedLenFeature(shape=1, dtype=tf.string),
-  'lug_boot': tf.FixedLenFeature(shape=1, dtype=tf.string),
-  'safety': tf.FixedLenFeature(shape=1, dtype=tf.string),
-}
-tf_example = tf.parse_example(serialized_tf_example, feature_configs)
-print(tf_example['buying'])
-tf_example_x = tf.concat([
-  tf.to_float(TBUYING.lookup(tf_example['buying'])),
-  tf.to_float(TMAINT.lookup(tf_example['maint'])),
-  tf.to_float(TDOORS.lookup(tf_example['doors'])),
-  tf.to_float(TPERSONS.lookup(tf_example['persons'])),
-  tf.to_float(TLUG_BOOT.lookup(tf_example['lug_boot'])),
-  tf.to_float(TSAFETY.lookup(tf_example['safety'])),
-], 1)
-
-print(tf_example['buying'])
-print(tf_example_x)
-x = tf.identity(tf_example_x, name='x')
+serialized = tf.placeholder(tf.string, name='tf_example')
+features = dict((k, tf.FixedLenFeature(shape=1, dtype=tf.string)) for k in FEATURES)
+tables = dict((k, tf.contrib.lookup.index_table_from_tensor(tf.constant(v))) for k, v in INPUTS.items())
+table = tf.contrib.lookup.index_to_string_table_from_tensor(tf.constant(OUTPUT))
+example = tf.parse_example(serialized, features)
+example_x = tf.concat([tf.to_float(tables[k].lookup(example[k])) for k in FEATURES], 1)
 # x = tf.placeholder(tf.float32, [None, inps])
-y_ = tf.placeholder(tf.float32, [None, outs])
+x = tf.identity(example_x, name='x')
+y_ = tf.placeholder(tf.float32, [None, OUTS])
 y = ann_network(x)
 cost_func = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-train_step = tf.train.GradientDescentOptimizer(rate).minimize(cost_func)
+train_step = tf.train.GradientDescentOptimizer(RATE).minimize(cost_func)
 values, indices = tf.nn.top_k(y, 4)
-table = tf.contrib.lookup.index_to_string_table_from_tensor(
-  tf.constant([NACCEPT[i] for i in range(4)])
-)
-prediction_classes = table.lookup(tf.to_int64(indices))
+classes = table.lookup(tf.to_int64(indices))
+
 
 print('\nstarting training:')
 if os.path.exists(MODEL):
@@ -120,12 +113,12 @@ if os.path.exists(MODEL):
 sess = tf.Session()
 bldr = tf.saved_model.Builder(MODEL)
 sess.run(tf.global_variables_initializer())
-for epoch in range(epochs):
+for epoch in range(EPOCHS):
   sess.run(train_step, {x: train_x, y_: train_y})
   pred = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
   accr = tf.reduce_mean(tf.cast(pred, tf.float32))
   accr_v = sess.run(accr, {x: train_x, y_: train_y})
   print('Epoch %d: %f accuracy' % (epoch, accr_v))
-signatures = {'serving_default': classify_signature(serialized_tf_example, y), 'predict': predict_signature(x, y)}
+signatures = {'serving_default': classify_signature(serialized, classes, values), 'predict': predict_signature(x, y)}
 bldr.add_meta_graph_and_variables(sess, ['serve'], signatures, main_op=tf.tables_initializer(), strip_default_attrs=True)
 bldr.save()
