ad6e911fadbf746ec1bd0eebd54fbf8810dc7c17
Subhajit Sahu <wolfram77@gmail.com>
2019-02-26 05:52:59 +0530

    client working

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ias-carevaluation
+
 Car Evaluation client and server for Hackathon.
+
+
+## setup
+
+```bash
+# build model
+python3 dev.py
+
+# deploy with tensorflow serving
+docker pull tensorflow/serving
+docker run -p 8501:8501 \
+  --mount type=bind,source=$PWD/build,target=/models/model \
+  -e MODEL_NAME=model -t tensorflow/serving
+
+# test with client
+python3 client.py
+```
+
+
+time to sleep!
--- client.py
+++ client.py
@@ -1,30 +1,49 @@
 #!/usr/bin/env python
-from client_stub import ClientStub
+from http.client import HTTPConnection
 import optparse
+import json
 
 
-class CarEvaluationService:
-  def __init__(self, addr=('127.0.0.1', 1992), serv=''):
-    self.predict_stub = ClientStub('str predict(str buying, str maint, str doors, str persons, str lug_boot, str safety)', addr, serv)
 
-  def predict(self, buying, maint, doors, persons, lug_boot, safety):
-    return self.predict_stub.call({'buying': buying, 'maint': maint, 'doors': doors, 'persons': persons, 'lug_boot': lug_boot, 'safety': safety})
+def parse_addr(addr):
+  i = addr.find(':')
+  host = '' if i<0 else addr[0:i]
+  port = int(addr if i<0 else addr[i+1:])
+  return (host, port)
+
+def input_json(buying, maint, doors, persons, lug_boot, safety):
+  return {'buying': buying, 'maint': maint, 'doors': doors,
+  'persons': persons, 'lug_boot': lug_boot, 'safety': safety}
+
+def classify(conn, model, method, example):
+  path = '/v1/models/%s:%s' % (model, method)
+  conn.request('POST', path, body=json.dumps({'examples': [example]}))
+  resp = conn.getresponse()
+  data = json.loads(resp.read())
+  if 'error' in data:
+    raise Exception(data['error'])
+  return data['results']
 
 
 p = optparse.OptionParser()
-p.set_defaults(host='192.168.43.36', port='1992')
-p.add_option('--host', dest='host', help='set remote host')
-p.add_option('--port', dest='port', help='set remote port')
+p.set_defaults(service='127.0.0.1:8501',model='model',method='classify')
+p.add_option('--service', dest='service', help='set tensorflow serving address')
+p.add_option('--model', dest='model', help='set model name to use')
+p.add_option('--method', dest='method', help='set method to use')
 (o, args) = p.parse_args()
 
-service = CarEvaluationService((o.host, int(o.port)), '')
-print('CarEvaluation inventory demo:\n')
+host, port = parse_addr(o.service)
+conn = HTTPConnection(host, port)
+print('CarEvaluation TF serving demo:\n')
 buying = input('buying price (vhigh, high, med, low):')
 maint = input('price of the maintenance (vhigh, high, med, low):')
 doors = input('number of doors (2, 3, 4, 5more):')
 persons = input('capacity in terms of persons to carry (2, 4, more):')
 lug_boot = input('the size of luggage boot (small, med, big):')
 safety = input('estimated safety of the car (low, med, high):')
-accept = service.predict(buying, maint, doors, persons, lug_boot, safety)
-print('result:', accept)
-print('Thanks for participating.')
+example = input_json(buying, maint, doors, persons, lug_boot, safety)
+
+print('\nresult:')
+results = classify(conn, o.model, o.method, example)
+for r in results[0]:
+  print('%s:' % r[0], r[1])
